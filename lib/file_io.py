dbd76abcdfa5c88d55954eb17875d0edddb20de2
KennedyShiku <kenrichshiku@gmail.com>
2023-12-13 20:20:54 +0300

    Completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(file_name.with_suffix(".txt"), mode = "w", encoding="utf-8") as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(file_name.with_suffix(".txt"), mode = "a", encoding="utf-8") as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(file_name.with_suffix(".txt"), mode = "r", encoding="utf-8") as f:
+        return f.read()
